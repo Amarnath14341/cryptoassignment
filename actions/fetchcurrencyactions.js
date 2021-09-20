69017bc2982b09ee09f59ec1fc6132562ebde8ca
Amarnath14341 <80396272+Amarnath14341@users.noreply.github.com>
2021-09-20 20:51:25 +0530

    Create fetchcurrencyactions.js

---
+++ actions/fetchcurrencyactions.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import {
+  FEATCH_CRYTO_CURRENCIES_FAILURE,
+  FEATCH_CRYTO_CURRENCIES_REQUEST,
+  FEATCH_CRYTO_CURRENCIES_SUCCESS,
+} from '../consts/cryptoRequestTypes';
+
+export const fetchCryptoCurrenciesRequest = () => {
+  return {
+    type: FEATCH_CRYTO_CURRENCIES_REQUEST,
+  };
+};
+
+export const fetchCryptoCurrenciesSuccess = (coins) => {
+  return {
+    type: FEATCH_CRYTO_CURRENCIES_SUCCESS,
+    payload: coins,
+  };
+};
+
+export const fetchCryptoCurrenciesFailuer = (error) => {
+  return {
+    type: FEATCH_CRYTO_CURRENCIES_FAILURE,
+    payload: error,
+  };
+};
+
+export const fetchCryptoCurrencies = (error) => {
+  return (dispatch) => {
+    dispatch(fetchCryptoCurrenciesRequest());
+    axios
+      .get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR')
+      .then((response) => {
+        const coins = response.data.coins;
+        dispatch(fetchCryptoCurrenciesSuccess(coins));
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        dispatch(fetchCryptoCurrenciesFailuer(errorMsg));
+      });
+  };
+};
