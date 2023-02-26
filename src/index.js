c1d5328d853163aa717e80ec7c002341df5dd077
Amarnath14341 <amarnath14341.acsree@gmail.com>
2023-02-26 20:37:42 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
@@ -16,9 +16,15 @@ const AppHome=()=>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
-  
+  </React.StrictMode> 
    root.render(<AppHome/>);
+
+// const root= ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
