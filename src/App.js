a57aacfe1263f30ddbaa6d4e08452f86b5422a0b
Amarnath14341 <amarnath14341.acsree@gmail.com>
2023-02-26 21:07:57 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Switch } from 'react-router-dom';
-import HomeContainer   from './components/HomeContainer';
-import WalletContainer from './components/WalletContainer';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+} from "react-router-dom";
+import HomeContainer from "./components/HomeContainer";
+import WalletContainer from "./components/WalletContainer";
 
 function App() {
   return (
     <Router>
-        <Routes path="/" exact component={HomeContainer} />
-        <Routes path="/wallet" component={WalletContainer} />
+      <Routes>
+        <Route path="/" element={<HomeContainer />} />
+        <Route path="/wallet" element={<WalletContainer />} />
+      </Routes>
     </Router>
   );
 }
